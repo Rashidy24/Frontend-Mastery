cb5586e7df51514cc857d7ce966ad8eaca38738f
Mostafa El-Rashidy <165313756+Rashidy24@users.noreply.github.com>
2025-02-10 19:10:56 +0200

    modified:   assets/Css/master.css         new file:   assets/Imgs/Bars.svg         modified:   assets/Js/main.js         deleted:    index..html         modified:   index.html

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -5,3 +5,16 @@ let myPageLoader = document.querySelector('[data-preloader]')
 window.addEventListener('load', function(){
     myPageLoader.style.display = "none"
 })
+
+const rvealedElements = document.querySelectorAll('[data-reveal]');
+const toDoFunc = function(){
+    for(let j = 0, len = rvealedElements.length; j < len; j++){
+        const element = rvealedElements[j];
+        element.getBoundingClientRect().top < window.innerHeight ? 
+        element.classList.add('revealed') 
+        : element.classList.remove('revealed');
+    }
+}
+
+window.addEventListener('scroll', toDoFunc);
+window.addEventListener('load', toDoFunc);
